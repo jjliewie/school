2800f72b7d85c87c8a3b266810089c651ac27641
jjliewie <rheej@ismanila.org>
2022-04-22 09:34:43 +0800

    linked list initial

---
+++ java.school/linkedlists/A.java
@@ -0,0 +1,31 @@
+package linkedlists;
+
+import java.util.LinkedList;
+
+public class A {
+
+    public static void main(String[] args) {
+        
+        LinkedList<String> demo = new LinkedList<String>();
+
+        demo.add("hello");
+        demo.add("sample");
+        demo.add("computer");
+        demo.add("science");
+
+        System.out.println(demo.size());
+
+        demo.addFirst("this is unique!");
+        demo.addLast("also this!");
+
+        System.out.println("first element: " + demo.getFirst());
+        System.out.println("last element: " + demo.getLast());
+
+        // these are also unique!
+
+        demo.removeFirst();
+        demo.removeLast();
+
+    }
+    
+}
